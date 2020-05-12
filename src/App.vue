f41348bcdf2b443fbcb043f0438d5a1fb8049507
Hayat Tamboli <hayat.tamboli@gmail.com>
2020-05-12 19:39:20 +0530

    added the side bar and header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :class="{dark: darkMode}">
+    <HeaderComp
+    :SideMenuProp="sideMenu"
+    @side_menu_toggle="sideMenu=!sideMenu"
+    ></HeaderComp>
+    <transition name="slide">
+      <SideMenu
+        v-show="sideMenu"
+        :DarkModeProp="darkMode"
+        @dark_mode="darkMode=!darkMode"
+      ></SideMenu>
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HeaderComp from "./components/HeaderComp";
+import SideMenu from "./components/SideMenu"
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HeaderComp,
+    SideMenu
+  },
+  data() {
+    return {
+      darkMode:false,
+      sideMenu:false
+    }
+  },
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+:root{
+  font-family: 'Jost', sans-serif;
+}
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.dark{
+  background: #122222;
+  color: white;
+  .far,.fa,.fas{
+    color: white;
+  }
+}
+#app{
+  width: 100%;
+  min-height: 100vh;
+  
+}
+.fa,.fas,.far{
+  font-size: 1.5rem;
+}
+.slide-enter-active {
+  transition: all .3s;
+}
+.slide-leave-active {
+  transition: all .3s
+}
+.slide-enter, .slide-leave-to {
+  transform: translateX(-10px);
+  opacity: 0;
 }
 </style>
