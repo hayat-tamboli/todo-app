3faeeeb0c16934438cd5e6d1954b88287e307186
Hayat Tamboli <hayat.tamboli@gmail.com>
2020-08-31 17:36:03 +0530

    🎨chaged theme

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    name: "Todone",
+    themeColor: "#00ADB5",
+    msTileColor: "#393E46",
+    manifestOptions: {
+      background_color: "#393E46"
+    }
+  }
+};
